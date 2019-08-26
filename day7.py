003677c32d8a1baec18bf80b1d5ab713532af94c
Asma Al-qahtani <asma@MacBook-Air-alkhas-b-Asma.local>
2019-08-26 14:07:01 -0700

    secondweek

---
+++ day7.py
@@ -0,0 +1,27 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #print() function:
+>>> print("Hi , Iam Asma")
+Hi , Iam Asma
+>>> print('Hi , Iam Asma')
+Hi , Iam Asma
+>>> #Assign string to a variable:
+>>> x="Hi , Iam Asma"
+>>> print(x)
+Hi , Iam Asma
+>>> #Multiline strings:
+>>> x="""Hi , Iam Asma , Iam studing new programming language called: python , i wish to be a professional to deal with it"""
+>>> print(x)
+Hi , Iam Asma , Iam studing new programming language called: python , i wish to be a professional to deal with it
+>>> #Strings are Arrays:
+>>> x="Hi, Iam Asma"
+>>> print(x[3])
+ 
+>>> print(x[2])
+,
+>>> print(x[4:6])
+Ia
+>>> print(x[8:12])
+Asma
+>>> 
