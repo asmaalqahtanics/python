d862a572914a4499b71fab3b8b37693cb8533068
Asma Al-qahtani <asma@MacBook-Air-alkhas-b-Asma.local>
2019-08-25 17:07:16 -0700

    fourthday

---
+++ day4.py
@@ -0,0 +1,28 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=3
+>>> b=2.7
+>>> c=15j
+>>> print(type(a))
+<class 'int'>
+>>> print(type(b))
+<class 'float'>
+>>> print(type(c))
+<class 'complex'>
+>>> d=-87.7e456
+>>> print(type(d))
+<class 'float'>
+>>> x=float(a)
+>>> y=int(b)
+>>> z=complex(d)
+>>> print(x,y,z)
+3.0 2 (-inf+0j)
+>>> import random
+>>> print (random.randrange(3,9))
+5
+>>> print (random.randrange(3,9))
+4
+>>> print (random.randrange(3,9))
+7
+>>> 
