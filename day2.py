d393bb685b829aba3ccff51f99c9048c91734bf4
Asma Al-qahtani <asma@MacBook-Air-alkhas-b-Asma.local>
2019-08-25 17:05:59 -0700

    secondday

---
+++ day2.py
@@ -0,0 +1,34 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # this is comment
+>>> print (" Hi Iam Asma")
+ Hi Iam Asma
+>>> print (" Hi Iam Asma") #This is comment
+ Hi Iam Asma
+>>> #print (" Hi Iam Asma")
+>>> print ("How Are You?")
+How Are You?
+>>> #This is comment
+>>> #written in
+>>> #more than just one line
+>>> print (" Hi Iam Asma")
+ Hi Iam Asma
+>>> """
+this is comment
+written in
+more than one line
+"""
+'\nthis is comment\nwritten in\nmore than one line\n'
+>>> ''' tjis is comment
+written in
+more than one line
+'''
+' tjis is comment\nwritten in\nmore than one line\n'
+>>>   '''
+SyntaxError: unexpected indent
+>>> ''' this is comment
+written in
+more than one line '''
+' this is comment\nwritten in\nmore than one line '
+>>> 
