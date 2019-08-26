02e9c4bb2c55b36f53196b2a64139cd0a2cbc975
Asma Al-qahtani <asma@MacBook-Air-alkhas-b-Asma.local>
2019-08-25 17:09:39 -0700

    second week

---
+++ day6.py
@@ -0,0 +1,20 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(3)
+>>> b=int(5.7)
+>>> c=int("456")
+>>> print(a,b,c)
+3 5 456
+>>> d=float(2)
+>>> e=float(4.7)
+>>> f=float("42")
+>>> print(d,e,f)
+2.0 4.7 42.0
+>>> #casting(string)
+>>> g=str("asma")
+>>> h=str("324")
+>>> i=str('3.42')
+>>> print(g,h,i)
+asma 324 3.42
+>>> 
